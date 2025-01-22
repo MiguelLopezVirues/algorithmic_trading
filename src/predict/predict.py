d9c04e665df2fed70ad843b2479c6270f4ca6d86
MiguelLopezVirues <miglop20@ucm.es>
2025-01-22 18:59:21 +0100

    Chore: Remove commented code from data_transformation

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -4,10 +4,24 @@ from fastapi import FastAPI
 import numpy as np
 import pandas as pd
 
+threshold = 0.6 # TO PUT INSIDE A CONFIG OR SIMILAR
 
 model = joblib.load("predict/mock_model.joblib")
 data = pd.read_parquet("predict/mock_data.parquet")
+class_names = np.array(["buy","sell"])
 
 app = FastAPI()
 
 @app.get("/")
+def reed_root():
+    return {"message":"API working"}
+
+@app.get("/predict")
+def predict(data: dict):
+    # pd.DataFrame(mock_data.to_dict(orient="records"))
+    data_to_predict = pd.DataFrame(data).values
+    
+    y_probs = model.predict_proba(data_to_predict)[:,1]
+
+    y_pred = 
+
